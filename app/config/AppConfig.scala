9ae9ecbb5deb31edb09c79fa1037aa29af1a4e90
Nic Fellows <nic@carboninternet.net>
2018-09-06 08:21:14 +0100

    PAS-140 Move to flat configuration

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -8,15 +8,16 @@ import uk.gov.hmrc.play.config.ServicesConfig
 
 @Singleton
 class AppConfig @Inject() (val runModeConfiguration: Configuration, environment: Environment) extends ServicesConfig {
+
   override protected def mode: Mode = environment.mode
 
   private def loadConfig(key: String) = runModeConfiguration.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
 
-  private val contactHost = runModeConfiguration.getString(s"$env.contact-frontend.host").getOrElse("")
-  private val contactFormServiceIdentifier = loadConfig(s"appName")
+  private val contactHost = runModeConfiguration.getString("contact-frontend.host").getOrElse("")
+  private val contactFormServiceIdentifier = loadConfig("appName")
 
-  lazy val analyticsToken = loadConfig(s"$env.google-analytics.token")
-  lazy val analyticsHost = loadConfig(s"$env.google-analytics.host")
+  lazy val analyticsToken = loadConfig("google-analytics.token")
+  lazy val analyticsHost = loadConfig("google-analytics.host")
   lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
 
--- app/services/http/WsAllMethods.scala
+++ app/services/http/WsAllMethods.scala
@@ -1,12 +1,12 @@
 package services.http
 
 import javax.inject.{Inject, Singleton}
-
-
 import play.api.{Configuration, Environment}
 import uk.gov.hmrc.http._
 import uk.gov.hmrc.play.audit.http.HttpAuditing
+import uk.gov.hmrc.play.audit.http.config.AuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+import uk.gov.hmrc.play.bootstrap.config.LoadAuditingConfig
 import uk.gov.hmrc.play.config.{AppName, RunMode}
 import uk.gov.hmrc.play.http.ws._
 
@@ -18,7 +18,7 @@ class WsAllMethods @Inject() (
   environment: Environment,
   config: Configuration
 ) extends WSHttp with HttpAuditing with AppName with RunMode {
-
+  
   override val hooks = Seq(AuditingHook)
 
   override val mode = environment.mode
