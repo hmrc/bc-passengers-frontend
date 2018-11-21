f2a6718cbd8d2463f9c1577ee182c727644022ac
thomascurry <thomas.curry@hotmail.com>
2018-11-21 10:41:23 +0000

    PAS-442 Use currency description in view instead of currency object

--- test/controllers/DashboardControllerSpec.scala
+++ test/controllers/DashboardControllerSpec.scala
@@ -1,7 +1,8 @@
 package controllers
 
-import models.JourneyData
+import models._
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
@@ -33,6 +34,7 @@ class DashboardControllerSpec extends BaseSpec {
 
   trait LocalSetup {
 
+    def travelDetailsJourneyData: JourneyData = JourneyData(euCountryCheck = Some("nonEuOnly"), ageOver17 = Some(true), privateCraft = Some(false))
     def cachedJourneyData: Option[JourneyData]
 
     def route[T](app: Application, req: Request[T])(implicit w: Writeable[T]): Option[Future[Result]] = {
@@ -44,34 +46,82 @@ class DashboardControllerSpec extends BaseSpec {
     }
   }
 
-  "Calling GET /bc-passengers-frontend/dashboard" should {
+  "Calling GET .../dashboard" should {
     "start a new session if any travel details are missing" in new LocalSetup {
 
-      override lazy val cachedJourneyData = Some(JourneyData(euCountryCheck = Some("nonEuOnly"), ageOver17 = Some(true), privateCraft = None))
+      override val cachedJourneyData: Option[JourneyData] = Some(travelDetailsJourneyData.copy(privateCraft = None))
 
-      val response = route(app, EnhancedFakeRequest("GET", "/bc-passengers-frontend/dashboard")).get
+      val result: Future[Result] = route(app, EnhancedFakeRequest("GET", "/bc-passengers-frontend/dashboard")).get
 
-      status(response) shouldBe SEE_OTHER
-      redirectLocation(response) shouldBe Some("/bc-passengers-frontend/new-session")
+      status(result) shouldBe SEE_OTHER
+      redirectLocation(result) shouldBe Some("/bc-passengers-frontend/new-session")
 
       verify(controller.travelDetailsService, times(1)).getJourneyData(any())
     }
   }
 
 
-  "respond with 200 and display the page is all travel details exist" in new LocalSetup {
+  "respond with 200 and display the page if all travel details exist" in new LocalSetup {
 
-    override lazy val cachedJourneyData = Some(JourneyData(euCountryCheck = Some("nonEuOnly"), ageOver17 = Some(true), privateCraft = Some(false)))
+    override val cachedJourneyData: Option[JourneyData] = Some(travelDetailsJourneyData)
 
-    val response = route(app, EnhancedFakeRequest("GET", "/bc-passengers-frontend/dashboard").withFormUrlEncodedBody("firstName" -> "Harry", "lastName" -> "Potter", "passportNumber" -> "801375812", "placeOfArrival" -> "Newcastle airport")).get
+    val result: Future[Result] = route(app, EnhancedFakeRequest("GET", "/bc-passengers-frontend/dashboard").withFormUrlEncodedBody("firstName" -> "Harry", "lastName" -> "Potter", "passportNumber" -> "801375812", "placeOfArrival" -> "Newcastle airport")).get
 
-    status(response) shouldBe OK
+    status(result) shouldBe OK
 
-    val content = contentAsString(response)
-    val doc = Jsoup.parse(content)
+    val content: String = contentAsString(result)
+    val doc: Document = Jsoup.parse(content)
 
     doc.getElementsByTag("h1").text() shouldBe "Tell us about your purchases"
     Option(doc.getElementById("start-again")) should not be None
 
+  }
+
+
+  "Calling GET .../calculation" should {
+    "redirect to the declaration done page if there is something to declare" in new LocalSetup {
+
+
+      override lazy val cachedJourneyData: Option[JourneyData] = Some(travelDetailsJourneyData.copy(
+        calculatorResponse = Some(CalculatorResponse(
+        Some(Alcohol(List(Band("B",List(Item("ALC/A1/CIDER", "1.00",None,Some(5), Calculation("1.00","1.00","1.00","3.00"),Metadata("5 litres cider", "Cider", "1.00",Currency("USD", "USA Dollar (USD)", Some("USD")), Country("United States of America (the)", "US", isEu = false, Some("USD")),
+          ExchangeRate("1.20", "2018-10-29")))), Calculation("1.00","1.00","1.00","3.00"))), Calculation("1.00", "1.00", "1.00", "3.00"))),
+        Some(Tobacco(Nil, Calculation("0.00", "0.00", "0.00", "0.00"))),
+        Some(OtherGoods(Nil, Calculation("0.00", "0.00", "0.00", "0.00"))),
+        Calculation("1.00", "1.00", "1.00", "3.00")
+      ))))
+
+      val result: Future[Result] = route(app, EnhancedFakeRequest("GET", "/bc-passengers-frontend/calculation")).get
+
+      status(result) shouldBe OK
+
+      val content: String = contentAsString(result)
+      val doc: Document = Jsoup.parse(content)
+
+      doc.getElementsByTag("h1").text shouldBe "You will need to pay £3.00for goods purchased outside of the EU"
+    }
+  }
+
+
+  "redirect to the nothing to declare done page if there is nothing to declare" in new LocalSetup {
+
+
+    override lazy val cachedJourneyData: Option[JourneyData] = Some(travelDetailsJourneyData.copy(
+      calculatorResponse = Some(CalculatorResponse(
+        Some(Alcohol(List(Band("B",List(Item("ALC/A1/CIDER", "1.00",None,Some(5), Calculation("1.00","1.00","1.00","3.00"),Metadata("5 litres cider", "Cider", "1.00",Currency("USD", "USA Dollar (USD)", Some("USD")), Country("United States of America (the)", "US", isEu = false, Some("USD")),
+          ExchangeRate("1.20", "2018-10-29")))), Calculation("0.00","0.00","0.00","0.00"))), Calculation("0.00", "0.00", "0.00", "0.00"))),
+        Some(Tobacco(Nil, Calculation("0.00", "0.00", "0.00", "0.00"))),
+        Some(OtherGoods(Nil, Calculation("0.00", "0.00", "0.00", "0.00"))),
+        Calculation("0.00", "0.00", "0.00", "0.00")
+      ))))
+
+    val result: Future[Result] = route(app, EnhancedFakeRequest("GET", "/bc-passengers-frontend/calculation")).get
+
+    status(result) shouldBe OK
+
+    val content: String = contentAsString(result)
+    val doc: Document = Jsoup.parse(content)
+
+    doc.getElementsByTag("h1").text shouldBe "You have nothing to declare"
   }
 }
