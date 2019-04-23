208a3641329ba84e7f3ffdd332463e4f46183f57
Nic Fellows <nic@carboninternet.net>
2019-04-23 10:53:21 +0100

    PAS-653 Add back links to declaration pages, disable browser cache

---
+++ app/filters/DisableBrowserCacheFilter.scala
@@ -0,0 +1,25 @@
+package filters
+
+import akka.stream.Materializer
+import javax.inject.Inject
+import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+class DisableBrowserCacheFilter @Inject()(
+  implicit val mat: Materializer,
+  implicit val ec: ExecutionContext
+) extends Filter {
+
+  def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+
+    nextFilter(requestHeader).map { result =>
+
+      result.withHeaders("Cache-Control" -> "no-cache, no-store, must-revalidate, max-age=0",
+        "Pragma" -> "no-cache",
+        "Expires" -> "0")
+    }
+  }
+
+}
