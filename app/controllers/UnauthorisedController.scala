e0c7801bfd50ce64892c16c8c6d7e5d2ede59dfa
“awasnik” <ankit.wasnik@digital.hmrc.gov.uk>
2020-11-17 10:34:07 +0000

    PAS-885: remove unsed inject

--- app/controllers/UnauthorisedController.scala
+++ app/controllers/UnauthorisedController.scala
@@ -16,19 +16,10 @@ import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import scala.concurrent.{ExecutionContext, Future}
 
 class UnauthorisedController @Inject()(
-                                        val countriesService: CountriesService,
-                                        val calculatorService: CalculatorService,
-                                        val travelDetailsService: TravelDetailsService,
-                                        val cache: Cache,
-                                        val productTreeService: ProductTreeService,
-                                        val error_template: views.html.error_template,
-                                        val currencyService: CurrencyService,
                                         val unauthorised_error_page: views.html.errors.unauthorised_error_page,
                                         override val controllerComponents: MessagesControllerComponents,
-                                        implicit val appConfig: AppConfig,
-                                        val backLinkModel: BackLinkModel,
                                         implicit val ec: ExecutionContext
-                                      ) extends FrontendController(controllerComponents) with I18nSupport with ControllerHelpers {
+                                      ) extends FrontendController(controllerComponents) with I18nSupport {
 
   def show: Action[AnyContent] = Action.async { implicit request =>
     Future.successful(
