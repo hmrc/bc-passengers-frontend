29af78e8663e58b6322fe80f0d718fa568ec4745
bharti5verma <68339135+bharti5verma@users.noreply.github.com>
2020-11-23 14:30:46 +0000

    PAS-1076 Populate Declaration Complete Screens with Items and Tax Breakdown

--- app/controllers/ZeroDeclarationController.scala
+++ app/controllers/ZeroDeclarationController.scala
@@ -9,33 +9,38 @@ import config.AppConfig
 import connectors.Cache
 import controllers.enforce.ZeroDeclarationAction
 import javax.inject.Inject
+import play.api.Logger
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Request}
-import services.PortsOfArrivalService
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.{CalculatorService, PortsOfArrivalService, ProductTreeService}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class ZeroDeclarationController @Inject()(
-    val cache: Cache,
-    val portsOfArrivalService: PortsOfArrivalService,
-    val error_template: views.html.error_template,
-    val isZeroDeclarationPage: views.html.declaration.zero_declaration,
-    zeroDeclarationAction: ZeroDeclarationAction,
-    override val controllerComponents: MessagesControllerComponents,
-    implicit val appConfig: AppConfig,
-    implicit val ec: ExecutionContext
-  ) extends FrontendController(controllerComponents) with I18nSupport {
-
-  implicit def convertContextToRequest(implicit localContext: LocalContext): Request[_] = localContext.request
+   val cache: Cache,
+   val portsOfArrivalService: PortsOfArrivalService,
+   val calculatorService: CalculatorService,
+   val productTreeService: ProductTreeService,
+   zeroDeclarationAction: ZeroDeclarationAction,
+   val error_template: views.html.error_template,
+   val isZeroDeclarationPage: views.html.declaration.zero_declaration,
+   override val controllerComponents: MessagesControllerComponents,
+   implicit val appConfig: AppConfig,
+   implicit val ec: ExecutionContext
+ ) extends FrontendController(controllerComponents) with I18nSupport with ControllerHelpers{
 
   def loadDeclarationPage(): Action[AnyContent] = zeroDeclarationAction { implicit context =>
 
     val placeOfArrivalValue = portsOfArrivalService.getDisplayNameByCode(context.getJourneyData.userInformation.get.selectPlaceOfArrival).getOrElse(context.getJourneyData.userInformation.get.enterPlaceOfArrival)
 
-    Future.successful {
-      Ok(isZeroDeclarationPage(context.getJourneyData.userInformation, context.getJourneyData.chargeReference.getOrElse(""), placeOfArrivalValue))
+    requireCalculatorResponse { calculatorResponse =>
+      Future.successful {
+        Ok(isZeroDeclarationPage(context.getJourneyData.userInformation, calculatorResponse, calculatorResponse.asDto(applySorting = false), context.getJourneyData.chargeReference.getOrElse(""), placeOfArrivalValue))
+
+      }
     }
   }
 
 }
+
