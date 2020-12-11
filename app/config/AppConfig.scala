9a4b829c2ac1bf981842bde3c3e43190a1b1bfec
anbu.roach <60651188+anburoach@users.noreply.github.com>
2020-12-11 13:08:42 +0000

    [PAS-1064]:Remove GTM Tagging and Add Tracking Consent Snippet

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -6,24 +6,24 @@
 package config
 
 import javax.inject.{Inject, Singleton}
+import play.api.Configuration
 import play.api.i18n.Lang
-import play.api.{Configuration, Environment}
+import play.api.mvc.Call
 import uk.gov.hmrc.play.bootstrap.config.{RunMode, ServicesConfig}
 
 @Singleton
-class AppConfig @Inject() (val runModeConfiguration: Configuration, environment: Environment, runMode: RunMode, servicesConfig: ServicesConfig) {
+class AppConfig @Inject() (val runModeConfiguration: Configuration, runMode: RunMode, servicesConfig: ServicesConfig) {
 
   private def loadConfig(key: String) = runModeConfiguration.get[String](key)
 
   private val contactHost = runModeConfiguration.getOptional[String]("contact-frontend.host").getOrElse("")
   private val contactFormServiceIdentifier = loadConfig("appName")
 
-  lazy val googleTagManagerId: String = loadConfig("google-tag-manager.id")
-  lazy val reportAProblemPartialUrl = runMode.envPath(s"contact/problem_reports_ajax?service=$contactFormServiceIdentifier")(other = contactHost)
-  lazy val reportAProblemNonJSUrl = runMode.envPath(s"/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier")(other = contactHost)
+  lazy val reportAProblemPartialUrl: String = runMode.envPath(s"contact/problem_reports_ajax?service=$contactFormServiceIdentifier")(other = contactHost)
+  lazy val reportAProblemNonJSUrl: String = runMode.envPath(s"/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier")(other = contactHost)
 
   lazy val betaFeedbackUrl = s"$contactHost/contact/beta-feedback-unauthenticated?service=$contactFormServiceIdentifier"
-  lazy val declareGoodsUrl = servicesConfig.getString("external.url")
+  lazy val declareGoodsUrl: String = servicesConfig.getString("external.url")
 
   lazy val maxOtherGoods: Int = runModeConfiguration.getOptional[Int]("max-other-goods-items").getOrElse(50)
   lazy val minPaymentAmount: Int = runModeConfiguration.getOptional[Int]("min-payment-amount").getOrElse(9)
@@ -38,8 +38,10 @@ class AppConfig @Inject() (val runModeConfiguration: Configuration, environment:
     "english" -> Lang("en"),
     "cymraeg" -> Lang("cy"))
 
-  def routeToSwitchLanguage = (lang: String) => controllers.routes.LocalLanguageController.switchToLanguage(lang)
+  def routeToSwitchLanguage: String => Call = (lang: String) => controllers.routes.LocalLanguageController.switchToLanguage(lang)
 
   lazy val languageTranslationEnabled: Boolean = runModeConfiguration.get[Seq[String]]("play.i18n.langs").contains("cy")
+
+  val gtmContainer: String = servicesConfig.getString("tracking-consent-frontend.gtm.container")
 
 }
