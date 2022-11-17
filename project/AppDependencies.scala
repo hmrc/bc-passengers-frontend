b106c2eb90158ed9547319f93f7573d6df774aff
EdCope <87376121+EdCope@users.noreply.github.com>
2022-11-17 17:12:01 +0000

    DDCE-3405: Revert layout changes and aligned input page

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,15 @@ import sbt._
 
 object AppDependencies {
 
-  val hmrcMongoVersion = "0.73.0"
+  val hmrcMongoVersion = "0.74.0"
   val compile          = Seq(
     ws,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.32.0-play-28",
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.9.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.4",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.33.0-play-28",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.11.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.0",
     "com.typesafe.play"            %% "play-json-joda"             % "2.9.3",
-    "uk.gov.hmrc"                  %% "play-language"              % "5.3.0-play-28",
+    "uk.gov.hmrc"                  %% "play-language"              % "5.4.0-play-28",
     "org.mindrot"                   % "jbcrypt"                    % "0.4",
     "org.webjars.npm"               % "accessible-autocomplete"    % "2.0.4",
     "ai.x"                         %% "play-json-extensions"       % "0.42.0"
