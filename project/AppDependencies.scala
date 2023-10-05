36acf695f3f97e0ab8f10080f2400caac89741e0
Victor Osipov <osipovvictor1994@gmail.com>
2023-10-05 10:58:19 +0100

    DDCE-4663 - delete unused property accessibility-statement

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
   private val compile        = Seq(
     ws,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.20.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.22.0-play-28",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.15.2",
     "com.typesafe.play"            %% "play-json-joda"             % "2.10.1",
@@ -21,7 +21,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-28" % hmrcMongoVersion,
     "org.scalatest"       %% "scalatest"               % "3.2.17",
     "org.jsoup"            % "jsoup"                   % "1.16.1",
-    "org.mockito"         %% "mockito-scala-scalatest" % "1.17.22",
+    "org.mockito"         %% "mockito-scala-scalatest" % "1.17.27",
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"
   ).map(_ % "test, it")
 
