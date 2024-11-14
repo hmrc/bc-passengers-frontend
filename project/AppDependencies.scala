8d8635cf525e35f104b28d3cd64a65839da41f64
Divya Manivannan <175815017+Divya-Manivannan-hmrc@users.noreply.github.com>
2024-11-14 11:47:58 +0000

    DDCE-6175 - Update FE to Use the Latest Version of Node (#466)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion     = "2.2.0"
+  private val hmrcMongoVersion     = "2.3.0"
   private val bootstrapPlayVersion = "9.5.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "10.13.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.3.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.18.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.18.1",
     "org.webjars.npm"               % "accessible-autocomplete"    % "3.0.0",
     "ai.x"                         %% "play-json-extensions"       % "0.42.0" //ExclusionRule in build.sbt for old play-json version
   )
