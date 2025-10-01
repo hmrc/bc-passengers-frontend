f1025ecc179086a821a1149458adf186bad92484
baznez <nesbeth@live.co.uk>
2025-10-01 08:37:18 +0100

    uk.gov.hmrc:play-frontend-hmrc-play-30 updated to 12.14.0 in project/AppDependencies.scala.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.13.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.14.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.20.0"
   )
