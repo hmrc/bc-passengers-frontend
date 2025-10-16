1e7e32e13c25b7eb85d8bb0f95763db238c37cad
BarryNez <84962272+BarryNez@users.noreply.github.com>
2025-10-16 14:45:52 +0100

    DDCE-7520 P1: Change "Confirm email address" Page #9577 DEV (#510)
    * amended CalculateDeclareController.scala
    amended dto.scala
    amended what_is_your_email.scala
    amended messages and messages.cy
    updated tests
    
    * updated tests
    
    * amended messages.cy
    
    * updated dependencies
    
    * amended errorSummary.scala.html

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion     = "2.9.0"
+  private val hmrcMongoVersion     = "2.10.0"
   private val bootstrapPlayVersion = "10.2.0"
 
   private val compile: Seq[ModuleID] = Seq(
