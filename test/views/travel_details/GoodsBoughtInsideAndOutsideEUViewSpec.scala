05306425cdd79acd289a4ddaa7e7acdcda812d77
erickbm6922 <47341247+erickbm6922@users.noreply.github.com>
2026-09-03 21:36:29 +0100

    DDCE-9154: group still and sparkling wine into one allowance on the Goods brought into GB or Isle of Man page, behind the wine-still-or-sparkling toggle

--- test/views/travel_details/GoodsBoughtInsideAndOutsideEUViewSpec.scala
+++ test/views/travel_details/GoodsBoughtInsideAndOutsideEUViewSpec.scala
@@ -130,4 +130,21 @@ class GoodsBoughtInsideAndOutsideEUViewSpec
       body should include("govuk-radios--inline")
     }
   }
+
+  "GoodsBoughtInsideAndOutsideEUView with the wine-still-or-sparkling toggle" should {
+
+    "render the merged wine allowance copy when the toggle is ON" in {
+      val view = injected[goods_bought_inside_and_outside_eu]
+        .apply(validForm, None)(request, messages, appConfigWith("features.wine-still-or-sparkling" -> true))
+      view.body should include(messages("text.gb.allowance.alc_2.still-or-sparkling"))
+      view.body should include(messages("text.gb.allowance.alc_1.still-or-sparkling"))
+    }
+
+    "render the original allowance copy when the toggle is OFF" in {
+      val view = injected[goods_bought_inside_and_outside_eu]
+        .apply(validForm, None)(request, messages, appConfigWith("features.wine-still-or-sparkling" -> false))
+      view.body should include(messages("text.gb.allowance.alc_2"))
+      view.body should not include messages("text.gb.allowance.alc_2.still-or-sparkling")
+    }
+  }
 }
