01c3380172f4e7c4c23465311b3c656c8b37f88f
baznez <nesbeth@live.co.uk>
2026-09-16 17:16:39 +0100

    amended messages amended messages.cy amended EUEvidenceControllerSpec.scala amended EUEvidenceItemViewSpec.scala

--- test/views/travel_details/EUEvidenceItemViewSpec.scala
+++ test/views/travel_details/EUEvidenceItemViewSpec.scala
@@ -73,8 +73,8 @@ class EUEvidenceItemViewSpec extends BaseViewSpec {
 
   "EUEvidenceItemView" when {
     renderViewTest(
-      title = "Evidence the item was made in the EU - Check tax on goods you bring into the UK - GOV.UK",
-      heading = "Evidence the item was made in the EU"
+      title = "Evidence the item was made in the EU (European Union) - Check tax on goods you bring into the UK - GOV.UK",
+      heading = "Evidence the item was made in the EU (European Union)"
     )
 
     "formWithErrors" should {
